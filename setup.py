f75ff16249b0cef9e0db5091cdeb8dcfa9bf600c
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2020-05-21 21:35:26 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='timesheet',
+    version='0.1.0a1',
+    author='Joseph Contreras',
+    author_email='',
+    description='Timesheet',
+    long_description='',
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+)
