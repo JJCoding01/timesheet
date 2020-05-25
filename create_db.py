190a715140f99accaad3e5235a4770a65271802d
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2020-05-25 15:29:15 -0500

    Add timesheet data; refactor random data generation out of populate

--- create_db.py
+++ create_db.py
@@ -1,10 +1,13 @@
+
 from pathlib import Path
 import csv
 import random, string
+import datetime
+from datetime import timedelta
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from timesheet.models import Base, Employee, Role, Project
+from timesheet.models import Base, Employee, Role, Project, Timesheet
 
 
 engine = create_engine("sqlite:///test.db", echo=False)
@@ -28,6 +31,32 @@ def get_random_string(word_count):
         [random_word(random.randrange(10)) for _ in range(word_count)]
     )
 
+def load_fake_roles():
+    return [Role(*role) for role in get_test_data("fake_roles.csv")]
+
+def load_fake_employees(roles):
+    employees = [
+        Employee(*name, role=roles[random.randrange(len(roles))])
+        for name in get_test_data("fake_names.csv")
+    ]
+    return employees
+
+def generate_fake_projects(n):
+    return [Project(n, get_random_string(15)) for n in range(1000, 1500)]
+
+def generate_fake_timesheets(n_weeks, n_projects, employees, projects):
+    timesheets = []
+    date = datetime.date(2020, 1, 1)
+    for week in range(n_weeks):
+        date += timedelta(days=7)
+        for k in range(n_projects):  # have 100 line items (projects/employee)
+            # get a tuple of hours for each day where the hours may be None for
+            # some of the days
+            days = tuple(random.randint(1, 9) if random.randint(0, 6) % 2 == 0 else None for _ in range(7))
+            employee = employees[random.randint(0, len(employees) - 1)]
+            project = projects[random.randint(0, len(projects) - 1)]
+            timesheets.append(Timesheet(date, employee, project, days))
+    return timesheets
 
 def recreate_all():
     Base.metadata.drop_all(engine)
@@ -35,16 +64,14 @@ def recreate_all():
 
 
 def populate():
+
+    roles = load_fake_roles()
+    employees = load_fake_employees(roles)
+    projects = generate_fake_projects(1000)
+    timesheets = generate_fake_timesheets(52, 100, employees, projects)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    roles = [Role(*role) for role in get_test_data("fake_roles.csv")]
-    employees = [
-        Employee(*name, role=roles[random.randrange(len(roles))])
-        for name in get_test_data("fake_names.csv")
-    ]
-    projects = [Project(n, get_random_string(15)) for n in range(1000, 1500)]
-
     for role in roles:
         session.add(role)
 
@@ -54,6 +81,9 @@ def populate():
     for project in projects:
         session.add(project)
 
+    for timesheet in timesheets:
+        session.add(timesheet)
+
     session.commit()
     session.close()
 
@@ -61,7 +91,6 @@ def populate():
 def main():
     recreate_all()
     populate()
-
 
 if __name__ == "__main__":
     main()
