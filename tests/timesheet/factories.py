052a31868bb0c735c75ee828024a46a94607b768
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2020-06-20 21:36:15 -0500

    Update parameters to be Sequences to easily create multiple objects

--- tests/timesheet/factories.py
+++ tests/timesheet/factories.py
@@ -11,33 +11,33 @@ class RoleFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = models.Role
 
-    title = fake.job()
-    description = fake.sentence(nb_words=10)
+    title = factory.Sequence(lambda n: fake.job())
+    description = factory.Sequence(lambda n: fake.sentence(nb_words=10))
 
 
 class ProjectFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = models.Project
 
-    number = fake.pyint(1000)
-    description = fake.sentence(nb_words=10)
-    active = fake.boolean(chance_of_getting_true=75)
+    number = factory.Sequence(lambda n: 1000 + n)
+    description = factory.Sequence(lambda n: fake.sentence(nb_words=5))
+    active = factory.Sequence(lambda n: fake.boolean(chance_of_getting_true=75))
 
 
 class GoalTypeFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = models.GoalType
 
-    type_ = fake.sentence(3)
-    description = fake.sentence(10)
+    type_ = factory.Sequence(lambda n: fake.sentence(3))
+    description = factory.Sequence(lambda n: fake.sentence(7))
 
 
 class EmployeeFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = models.Employee
 
-    first_name = fake.first_name()
-    last_name = fake.last_name()
+    first_name = factory.Sequence(lambda n: fake.first_name())
+    last_name = factory.Sequence(lambda n: fake.last_name())
     role = factory.SubFactory(RoleFactory)
 
 
@@ -57,7 +57,7 @@ class GoalFactory(factory.alchemy.SQLAlchemyModelFactory):
         model = models.Goal
 
     goal = "\n".join(fake.paragraphs(nb=4))
-    ending_date = datetime.strptime(fake.date(), "%Y-%m-%d")
+    ending_date = factory.Sequence(lambda n: datetime.strptime(fake.date(), "%Y-%m-%d"))
     type_ = factory.SubFactory(GoalTypeFactory)
     employee = factory.SubFactory(EmployeeFactory)
 
