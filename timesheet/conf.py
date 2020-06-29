e06ab6c423408c288dbc640fb34355f72fe518b4
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2020-06-28 22:46:42 -0500

    Add engine and session to conf to be imported when needed
    This is done so the whole app can easily use the same session object.

--- timesheet/conf.py
+++ timesheet/conf.py
@@ -1,2 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 DATABASE_URI = "postgresql://postgres:admin@localhost/timesheets"
 EMAIL_DOMAIN = "@example.com"
+
+engine = create_engine(DATABASE_URI, echo=False)
+Session = sessionmaker(bind=engine)
+
